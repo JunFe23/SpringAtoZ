d01b6803616e1fde1d5d074f7a43f0503910792a
JunFe23 <ogum0405@hanmail.net>
2021-08-30 01:27:48 +0900

    2021.08.29 ~ 4장

---
+++ ex00/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,36 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		
+		try (Connection con = DriverManager.getConnection(
+				"jdbc:oracle:thin:@10.211.55.7:1521:ORCL",
+				"book_ex",
+				"book_ex")){
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
