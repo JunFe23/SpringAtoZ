ea26cef133df8a545d4eff59999cdc5c3903ff52
JunFe23 <ogum0405@hanmail.net>
2021-11-28 15:38:35 +0900

    2021.11.28

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,7 +1,12 @@
 package org.zerock.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.odmain.SampleDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -16,6 +21,31 @@ public class SampleController {
 		
 		log.info("basic..........");
 	
+	}
+	
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+		
+		log.info("" + dto);
+		
+		return "ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+		
+		log.info("name: " + name);
+		log.info("age :" + age);
+		
+		return "ex02";
+	}
+	
+	@GetMapping("/ex02list")
+	public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+		
+		log.info("ids : " + ids);
+		
+		return "ex02List";
 	}
 
 }
--- ex01/src/main/java/org/zerock/odmain/SampleDTO.java
+++ ex01/src/main/java/org/zerock/odmain/SampleDTO.java
@@ -4,5 +4,7 @@ import lombok.Data;
 
 @Data
 public class SampleDTO {
-
+	
+	private String name;
+	private int age;
 }
