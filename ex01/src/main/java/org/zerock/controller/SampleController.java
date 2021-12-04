03184e6b81447acff5206ff7aefa6a4b5dfdcfb1
JunFe23 <ogum0405@hanmail.net>
2021-12-04 13:55:25 +0900

    2021.11.04 ~@modelAttribute

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
@@ -84,7 +85,11 @@ public class SampleController {
 	}
 	
 	@GetMapping("/ex04")
-	public String ex04(SampleDTO dto, int page) {
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
 		
+		log.info("dto: " + dto);
+		log.info("page: " + page);
+		
+		return "/sample/ex04";
 	}
 }
